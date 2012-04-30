84501b307e538518886988ce4d8aafc0d823141e
Emmi Otava <emirir5@gmail.com>
2012-04-30 09:58:35 +0300

    scripts updated

--- Sorter/start.sh
+++ Sorter/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar target/Sorter-1.0-SNAPSHOT.jar /home/fs/kerola/rio_testdata/uint64-keys.bin results.txt 1000
+java -jar target/Sorter-1.0-SNAPSHOT.jar /home/fs/kerola/rio_testdata/uint64-keys.bin results-parallel.txt 1000 parallel
